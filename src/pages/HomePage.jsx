fc1904774afd0889b4ffb3733ff32399583ed36b
fatihozcandev <fatih-ozcan@windowslive.com>
2024-05-01 01:38:17 +0200

    editors pick has been placed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import BuyMe from "../components/homepageComponents/BuyNow";
 import FeaturedPosts from "../components/homepageComponents/FeaturedPosts";
 import BestSeller from "../components/homepageComponents/BestSeller";
+import EditorsPick from "../components/homepageComponents/EditorsPick";
 import Slider1 from "../components/reusableComponents/Slider1";
 import { topSliderData, bottomSliderData } from "../data/data";
 
@@ -9,6 +10,7 @@ function HomePage() {
   return (
     <div className="mx-auto">
       <Slider1 sliderData={topSliderData} />
+      <EditorsPick />
       <BestSeller />
       <Slider1 sliderData={bottomSliderData} />
       <BuyMe />
